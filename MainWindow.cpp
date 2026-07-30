200ab90725016324d6c6508354e29ba17323ef7f
lwm <2997504841@qq.com>
2026-07-31 00:00:02 +0800

    完成解封装demux

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -15,3 +15,42 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnPlay_clicked()
+{
+    if(m_demuxThread){
+        // m_demuxThread->requestInterruption();
+        m_demuxThread->stopThread();
+        m_demuxThread->wait();
+        m_demuxThread->finiDemuxThread();
+        delete m_demuxThread;
+        m_demuxThread = nullptr;
+    }
+    qDebug()<<"已清空解封装线程";
+    if(playerCtx){
+        delete playerCtx;
+        playerCtx = nullptr;
+    }
+    qDebug()<<"已清空playerCtx";
+
+    // 检查文件是否存在
+    QFileInfo fileInfo(windowTitle());
+    if (!fileInfo.exists()) {
+        QMessageBox::warning(this, "warning", "请拖入有效的文件");
+        return;
+    }
+
+    /* 为新的视频文件初始化播放器结构体 */
+    playerCtx = new FFmpegPlayerCtx;
+    // 获取输入文件信息
+    playerCtx->iFile = fileInfo;
+    // 初始化解封装线程
+    m_demuxThread = new MyDemuxThread;
+    m_demuxThread->setPlayerCtx(playerCtx);
+    if (m_demuxThread->initDemuxThread() != 0) {
+        qDebug()<< "DemuxThread init Failed.";
+        return;
+    }
+
+    m_demuxThread->start();
+}
--- MainWindow.h
+++ MainWindow.h
@@ -3,12 +3,16 @@
 
 #include <QFileInfo>
 #include <QMainWindow>
+#include <QMessageBox>
+#include "MyDemuxThread.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+struct FFmpegPlayerCtx;
 
 class MainWindow : public QMainWindow
 {
@@ -18,7 +22,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnPlay_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    FFmpegPlayerCtx *playerCtx = nullptr;
+
+    MyDemuxThread *m_demuxThread = nullptr;
 };
 #endif // MAINWINDOW_H
