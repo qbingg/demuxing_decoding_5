ff55aa2c9a2167f0cbe924453cfa74a34a44f0c4
lwm <2997504841@qq.com>
2026-08-01 21:48:01 +0800

    添加：MyAudioDecodeThread.h，去掉文件中不需要的代码

---
+++ MyAudioDecodeThread.cpp
@@ -0,0 +1,3 @@
+#include "MyAudioDecodeThread.h"
+
+MyAudioDecodeThread::MyAudioDecodeThread() {}
---
+++ MyAudioDecodeThread.h
@@ -0,0 +1,45 @@
+#ifndef MYAUDIODECODETHREAD_H
+#define MYAUDIODECODETHREAD_H
+
+#include <QDebug>
+#include <QThread>
+#include <SDL3/SDL.h>
+
+struct AVCodecContext;
+struct AVPacket;
+struct AVFrame;
+struct FFmpegPlayerCtx;
+
+class MyAudioDecodeThread : public QThread
+{
+    Q_OBJECT
+public:
+    explicit MyAudioDecodeThread(QObject *parent = nullptr);
+    ~MyAudioDecodeThread();
+    void setPlayerCtx(FFmpegPlayerCtx *ctx);
+    void stopThread();
+
+    /**
+     * 解码音视频数据包，将AVPacket解析为AVFrame
+     * @param dec 解码器上下文，怎么来的请看open_codec_context()
+     * @param pkt 输入包
+     * @param frame 一个包有多个帧，并不是最终输出帧，只是作为载体，全局变量不用重复new AVFrame
+     * @return 解码结果状态码
+     */
+    int decode_packet(AVCodecContext *dec, const AVPacket *pkt ,AVFrame *frame);
+
+    void getAudioData(unsigned char *stream, int len);
+
+signals:
+    void sendMessage();
+    void sendpcmPeakBar(double,int16_t,int16_t);
+
+private:
+    FFmpegPlayerCtx *is = nullptr;
+
+    std::atomic<bool> m_stop = 0;
+protected:
+    void run()override;
+};
+
+#endif // MYAUDIODECODETHREAD_H
